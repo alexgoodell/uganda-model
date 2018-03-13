85443321b9e55d054f66801224bb20f1faa75211
alexgoodell <alexgoodell@gmail.com>
2018-03-13 15:05:58 -0700

    pre-auto-fetch-archrive

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,9 @@
 import bibtexparser
 import pdfkit
-
-
-
+from fabric.api import run, sudo, local, settings
+import logging
+import sys
+logging.basicConfig()
 
 
 
@@ -10,7 +11,21 @@ def archive_test():
 	pdfkit.from_url('https://www.google.com/', 'out.pdf')
 
 def archive_all_citations():
-	
 
-#with open('bibtex.bib') as bibtex_file:
-#   bibtex_database = bibtexparser.load(bibtex_file)
+	with open('refs/library.bib') as bibtex_file:
+		bibtex_database = bibtexparser.load(bibtex_file)
+
+	for i, entry in enumerate(bibtex_database.entries):
+		if 'url' in entry:
+			cite_key = str(entry['ID'])
+			url = str(entry['url'])
+			cmd = "archivenow --ia {}".format(url)
+			archive_url = local(cmd)
+			bibtex_database.entries[i]["archive-url"] = archive_url
+
+			print "Archived as {} \n\n".format(archive_url)
+			
+
+	with open('bibtex.bib', 'w') as bibtex_file:
+		bibtexparser.dump(bibtex_database, bibtex_file)
+
