cac8c79e563a84649769eb5e562954f8e4dae573
alexgoodell <alexgoodell@gmail.com>
2018-03-13 23:33:22 -0700

    plotting

--- tmp.py
+++ tmp.py
@@ -24,3 +24,10 @@
 	'award04': 'program_degree5',
 	'country04': 'country_degree5',
 	'year04': 'year_degree5'
+
+
+
+
+'unnamed:_20': 'concat_degree3', 'profes02': 'degree_degree3', 'award02': 'program_degree3', 'country02': 'country_degree3', 'year02': 'year_degree3',
+'unnamed:_25': 'concat_degree4','profes03': 'degree_degree4','award03': 'program_degree4','country03': 'country_degree4','year03': 'year_degree4',
+'unnamed:_30': 'concat_degree5', 'profes04': 'degree_degree5', 'award04': 'program_degree5', 'country04': 'country_degree5', 'year04': 'year_degree5'
